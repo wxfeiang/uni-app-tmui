6ede1e27efc0d1959b47393a19fd1941aeee251d
wxfeiang <wxfeiang@qq.com>
2024-02-14 14:55:22 +0800

    feat: init

--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,31 @@
-import { defineConfig } from "vite";
-import uni from "@dcloudio/vite-plugin-uni";
-import vueJsx from "@vitejs/plugin-vue-jsx";
+import uni from "@dcloudio/vite-plugin-uni"
+import vueJsx from "@vitejs/plugin-vue-jsx"
 import { resolve } from "path"
-// import Components from 'unplugin-vue-components/vite'
-// https://vitejs.dev/config/
+import { defineConfig } from "vite"
+
 export default defineConfig({
-	build: {
-		target: "es6"
-	},
-	resolve: {
-		alias: [
-			{
-				find: "@",
-				replacement: resolve(__dirname, 'src')
-			}
-		]
-	},
-	server: {
-		port: 1314,
-		// 选项写法
-		proxy: {
-			'/pag': {
-				target: 'https://cdn.tmui.design',
-				changeOrigin: true,
-				rewrite: (path) => path.replace(/^\/api/, '/api')
-			},
-		}
-	},
-	plugins: [
-		uni(),
-		vueJsx()
-	]
-});
+  build: {
+    target: "es6"
+  },
+  resolve: {
+    alias: [
+      {
+        find: "@",
+        replacement: resolve(__dirname, "src")
+      }
+    ]
+  },
+  server: {
+    port: 8890,
+    // 选项写法
+    proxy: {
+      "/pag": {
+        target: "https://cdn.tmui.design",
+        changeOrigin: true,
+        rewrite: (path) =>
+          path.replace(/^\/api/, "/api")
+      }
+    }
+  },
+  plugins: [uni(), vueJsx()]
+})
