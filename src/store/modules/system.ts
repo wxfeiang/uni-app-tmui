b2ccb207478c9f43f2bf5ddc79a528930f55e2cc
wxfeiang <mac@wangpengdeMacBook-Pro-2017.local>
2024-05-07 16:39:58 +0800

    fix: 修改查询

--- src/store/modules/system.ts
+++ src/store/modules/system.ts
@@ -10,41 +10,50 @@ interface STSCONFIG {
   type: string;
 }
 
-export const useSystemStore = defineStore("systemStore", () => {
+export const useSystemStore = defineStore("systemStore",
+  () => {
 
-  const appSecret = ref("");
+    const appSecret = ref("");
 
-  const resstrppd = ref(""); // 解密
-  const filterData = ref(<STSCONFIG>{});
-  const userDId = ref("");
-  const dot = ref("");
+    const resstrppd = ref(""); // 解密
+    const filterData = ref(<STSCONFIG>{});
+    const userDId = ref("");
+    const dot = ref("");
 
-  function RESSTRPPD(value: string) {
-    resstrppd.value = value;
-  }
-
-  function fILTERDATA(value: STSCONFIG) {
-    filterData.value = value;
-  }
-  function DOT(value: string) {
-    dot.value = value;
-  }
-
-  function initSystemInfo() {
-    if (!resstrppd.value || !filterData.value || !dot.value) {
-      useSystem(true)
+    function RESSTRPPD(value: string) {
+      resstrppd.value = value;
     }
-  }
-  return {
-    RESSTRPPD,
-    fILTERDATA,
-    DOT,
 
-    appSecret,
-    resstrppd,
-    filterData,
-    userDId,
-    dot,
-    initSystemInfo,
-  };
-});
+    function fILTERDATA(value: STSCONFIG) {
+      filterData.value = value;
+    }
+    function DOT(value: string) {
+      dot.value = value;
+    }
+
+    function initSystemInfo() {
+      if (!resstrppd.value || !filterData.value || !dot.value) {
+        useSystem(true)
+      }
+    }
+    return {
+      RESSTRPPD,
+      fILTERDATA,
+      DOT,
+
+      appSecret,
+      resstrppd,
+      filterData,
+      userDId,
+      dot,
+      initSystemInfo,
+    }
+
+  },
+  // {
+  //   persist: {
+  //     // 持久化存储写法
+  //     enabled: true,
+  //   },
+  // },
+);
